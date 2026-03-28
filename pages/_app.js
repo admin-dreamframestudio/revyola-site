182ed3637fd62e843f8c6707d6d662f019e8f7f1
admin-dreamframestudio <210484944+admin-dreamframestudio@users.noreply.github.com>
2026-03-27 17:46:48 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import "../styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Revyola — Payment Intelligence</title>
+        <meta name="description" content="Premium signal intelligence for healthcare payments" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
